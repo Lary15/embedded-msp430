b03c55950019c06df78160542284faf7e34c197e
Lary15 <lari1415@hotmail.com>
2021-11-10 22:47:34 -0300

    Add - Configure Cargo.toml
    - Add extern crates
    - Add mspdebug.gdb file
    - Change code on main.rs to blink led example

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+#![feature(array_value_iter)]
+#![no_std]
+#![no_main]
+
+extern crate panic_msp430;
+
+use msp430::asm;
+use msp430_rt::entry;
+
+use msp430_periph::devices::msp430fr5969::MSP430FR5969;
+use msp430_periph::peripherals::{pmm_4 as pmm, portb_3i1 as p1, portb_3i2 as p4, watchdog_timer_2 as wdt};
+use msp430_periph::utils::Value;
+
+#[entry]
+fn main() -> ! {
+    let mut p: MSP430FR5969 = unsafe { core::mem::transmute(()) };
+
+    // Disable watchdog
+    p.watchdog_timer
+        .ctl
+        .write(unsafe { Value::from_raw(0x5a00) } | wdt::HOLD(true));
+
+    // Set P1.0 and P4.6 as output
+    p.port_1.out.modify(p1::OUT0(false));
+    p.port_4.out.modify(p4::OUT6(true));
+    p.port_1.dir.modify(p1::DIR0(true));
+    p.port_4.dir.modify(p4::DIR6(true));
+
+    loop {
+        let mut i = 0u16;
+        while i < 10_000u16 {
+            i += 1;
+            asm::nop();
+        }
+
+        // Toggle outputs
+        p.port_1.out.toggle(p1::OUT::OUT0);
+        p.port_4.out.toggle(p4::OUT::OUT6);
+    }
+}
+
+#[no_mangle]
+extern "C" fn abort() -> ! {
+    panic!();
+}
